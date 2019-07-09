803a818c10323014a5864780ebf41365cb0d5c2e
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-09 11:16:35 -0700

    weather

--- server.js
+++ server.js
@@ -14,12 +14,24 @@ app.use(cors());
 
 // Location Route
 app.get('/location', (request, response) => {
-  console.log(request.query.data);
+  // console.log(request.query.data);
   try {
     const locationData = searchToLatLng(request.query.data);
     response.send(locationData);
   }
-  catch (e){
+  catch (e) {
+    response.status(500).send('Status 500, not functional.');
+  }
+});
+
+//Weather Route
+app.get('/weather', (request, response) => {
+  console.log(request.query.data);
+  try {
+    const weatherData = getWeatherRoute(request.query.data);
+    response.send(weatherData);
+  }
+  catch (e) {
     response.status(500).send('Status 500, not functional.');
   }
 });
@@ -44,8 +56,31 @@ function searchToLatLng(locationName) {
     geoData.results[0].geometry.location.lat,
     geoData.results[0].geometry.location.lng
   );
-  console.log(location);
+  // console.log(location);
   return location;
+}
+
+//Weather constructor 
+// function Weather(forecastArr, timeArr) {
+//   for (let i = 0; i < timeArr.length; i++) {
+//     this.days.push(new Day(forecastArr[i], timeArr[i]));
+//   }
+// }
+function Day(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
+}
+
+function getWeatherRoute(locationName) {
+  const weatherData = require('./data/darksky.json');
+  let retArr = [];
+  console.log(weatherData.daily.data);
+  for (let el of weatherData.daily.data) {
+    let time = new Date(el.time);
+    retArr.push(new Day (el.summary, time));
+  }
+  console.log(retArr);
+  return retArr;
 }
 
 // Start the server.
